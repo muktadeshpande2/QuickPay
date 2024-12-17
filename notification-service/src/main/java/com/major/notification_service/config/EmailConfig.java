f1418b9c939e8cc3a69e4647d373f116cd88f4bb
Mukta Deshpande <muktadeshpande000@gmail.com>
2024-12-17 18:42:33 +0530

    notification-service

---
+++ notification-service/src/main/java/com/major/notification_service/config/EmailConfig.java
@@ -0,0 +1,32 @@
+package com.major.notification_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    JavaMailSenderImpl getEmailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp@gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("m.desh7903@gmail.com");
+        javaMailSender.setPassword("qwertyuiopasdf");
+
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.debug", true);
+        properties.put("mail.smtp.starttls.enable", true);
+
+        return javaMailSender;
+    }
+
+    @Bean
+    SimpleMailMessage getMailMessage() {
+        return new SimpleMailMessage();
+    }
+}
---
+++ notification-service/src/main/java/com/major/notification_service/service/NotificationService.java
@@ -0,0 +1,72 @@
+package com.major.notification_service.service;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.json.simple.parser.ParseException;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    SimpleMailMessage simpleMailMessage;
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = {"transaction_completed"}, groupId = "qp2")
+    public void notify(String msg) throws ParseException {
+        JSONObject jsonObject = (JSONObject) new JSONParser().parse(msg);
+        String transactionId = (String) jsonObject.get("transactionId");
+        String transactionStatus = (String) jsonObject.get("transactionStatus");
+        Long amount = (Long) jsonObject.get("amount");
+        String senderEmail = (String) jsonObject.get("senderEmail");
+        String receiverEmail = (String) jsonObject.get("receiverEmail");
+
+        String senderMsg = getSenderMessage(transactionStatus, amount, transactionId);
+        String receiverMsg = getReceiverMessage(transactionStatus, amount, senderEmail);
+
+
+        if(!senderMsg.isEmpty()) {
+            simpleMailMessage.setTo(senderEmail);
+            simpleMailMessage.setSubject("E-Wallet Transaction Update");
+            simpleMailMessage.setFrom("m.desh7903@gmail.com");
+            simpleMailMessage.setText(senderMsg);
+            javaMailSender.send(simpleMailMessage);
+        }
+
+        if(!receiverMsg.isEmpty()) {
+            simpleMailMessage.setTo(receiverEmail);
+            simpleMailMessage.setSubject("E-Wallet Transaction Update");
+            simpleMailMessage.setFrom("m.desh6541@hotmail.com");
+            simpleMailMessage.setText(senderMsg);
+            javaMailSender.send(simpleMailMessage);
+        }
+    }
+
+    private String getSenderMessage(String transactionStatus, Long amount, String transactionId) {
+        String msg = " ";
+
+        if(transactionStatus.equals("FAILURE")) {
+            msg = "Hi! Your transaction of amount" + amount + " , transactionId = " + transactionId + " has failed";
+        } else {
+            msg = "Hi! Your account has been debited with amount " + amount + " , transactionId = " + transactionId;
+        }
+
+        return msg;
+    }
+
+    private String getReceiverMessage(String transactionStatus, Long amount, String senderEmail) {
+        String msg = " ";
+
+        if(transactionStatus.equals("SUCCESSFUL")) {
+            msg = "Hi! Your account has been credited with amount " + amount + " for the transaction done by the user " + senderEmail;
+        }
+
+        return msg;
+    }
+}
